c88f19f20709493ec15148b99a600c79e2858628
Daniel <itzdanielmichael@gmail.com>
2022-09-13 22:50:42 -0400

    format

--- commands/opgg-search.js
+++ commands/opgg-search.js
@@ -18,10 +18,7 @@ module.exports = {
 
       const rank = `${data.solo_tier_info.tier} ${data.solo_tier_info.division} ${data.solo_tier_info.lp}LP`;
       return message.channel.send(
-        `
-        Name: ${data.name}
-        Level: ${data.level} 
-        Rank: ${rank}`,
+        `Name: ${data.name}\nLevel: ${data.level}\nRank: ${rank}`,
         { files: [data.profile_image_url] }
       );
     } catch (error) {
